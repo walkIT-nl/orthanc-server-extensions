18802c45e7901054bfc86810585a57dea7b8e13c
Walco van Loon <walco@walkit.nl>
2021-01-15 21:37:01 +0100

    Introduce resource type (mistakenly introduced as level)

--- orthanc_ext/event_dispatcher.py
+++ orthanc_ext/event_dispatcher.py
@@ -6,11 +6,11 @@ def register_event_handlers(event_handlers, orthanc_module):
     @dataclass
     class Event:
         change_type: int
-        level: int
+        resource_type: int
         resource_id: str
 
         def __str__(self):
-            return f"Event(change_type={event_types.get(self.change_type)}, level={self.level}, resource_id='{self.resource_id}')"
+            return f"Event(change_type={event_types.get(self.change_type)}, resource_type={self.resource_type}, resource_id='{self.resource_id}')"
 
     def ensure_iterable(v):
         return v if isinstance(v, Iterable) else [v]
@@ -27,10 +27,10 @@ def register_event_handlers(event_handlers, orthanc_module):
     def unhandled_event_logger(event, orthanc):
         orthanc.LogInfo(f'no handler registered for {event_types[event.change_type]}')
 
-    def OnChange(change_type, level, resource_id):
+    def OnChange(change_type, resource_type, resource_id):
         handlers = event_handlers.get(change_type, [unhandled_event_logger])
         for handler in handlers:
-            event = Event(change_type, level, resource_id)
+            event = Event(change_type, resource_type, resource_id)
             handler(event, orthanc=orthanc_module)
 
     orthanc_module.RegisterOnChangeCallback(OnChange)
