ef65be828fc8e86d6700133966f0cf4446b76d30
Walco van Loon <walco@walkit.nl>
2021-01-30 16:43:01 +0100

    Improvements to function call examples

--- tests/entry_point.py
+++ tests/entry_point.py
@@ -1,13 +1,15 @@
 """Test entry point script for Orthanc Python Plugin."""
+import json
+
 from orthanc_ext import event_dispatcher
 
 # provided by the plugin runtime.
 import orthanc
 
 
-def log_event():
+def log_event(param):
     def log_event_impl(event, orthanc):
-        orthanc.LogWarning(f"orthanc '{event}' event handled!")
+        orthanc.LogWarning(f"orthanc '{event}' event handled with param {param}")
 
     return log_event_impl
 
@@ -17,6 +19,6 @@ def start_maintenance_cycle(event, orthanc):
 
 
 event_dispatcher.register_event_handlers({
-    orthanc.ChangeType.ORTHANC_STARTED: [log_event(), start_maintenance_cycle()],
-    orthanc.ChangeType.ORTHANC_STOPPED: log_event()
+    orthanc.ChangeType.ORTHANC_STARTED: [log_event("started"), start_maintenance_cycle],
+    orthanc.ChangeType.ORTHANC_STOPPED: log_event("stopped")
 }, orthanc_module=orthanc)
