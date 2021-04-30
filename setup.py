c6d05639b5978f36b2ac3bd85acb3c60e6dd981b
Walco van Loon <walco@walkit.nl>
2021-04-30 10:26:02 +0200

    Bump version: 3.1.8 → 3.1.9

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/walkIT-nl/orthanc-server-extensions',
-    version='3.1.8',
+    version='3.1.9',
     zip_safe=False,
 )
