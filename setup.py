873798a0a95cc733b922ed12512fb3bc50c78cd6
walcovanloon <w.vanloon@quin.md>
2022-09-13 19:25:22 +0200

    Drop python 3.7 support, introduce 3.10 support

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ classifiers = [
     'License :: OSI Approved :: GNU Affero General Public License v3',
     'Natural Language :: English',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
