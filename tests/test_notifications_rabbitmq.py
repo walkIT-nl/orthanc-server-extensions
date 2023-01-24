8b9ba4871111c293e7a5fb57132377b70dca31f2
walcovanloon <walco@walkit.nl>
2023-01-24 16:21:52 +0100

    #131 Add tests for async event notification through RabbitMQ

--- tests/test_notifications_rabbitmq.py
+++ tests/test_notifications_rabbitmq.py
@@ -1,8 +1,17 @@
 import time
 
 import aio_pika
+import httpx
 import pytest
 from dockercontext import container as containerlib
+
+from orthanc_ext import event_dispatcher
+from orthanc_ext.orthanc import OrthancApiHandler
+
+
+@pytest.fixture(scope='session')
+def orthanc():
+    yield OrthancApiHandler()
 
 
 @pytest.fixture(scope='session')
@@ -13,25 +22,56 @@ def docker_rabbitmq():
         yield container
 
 
-@pytest.mark.asyncio
-async def test_create_queue(docker_rabbitmq):
+async def create_queue(*_):
     connection = await aio_pika.connect_robust('amqp://guest:guest@127.0.0.1:55672/')
     try:
         queue_name = 'orthanc-events'
         channel = await connection.channel()
-        queue = await channel.declare_queue(queue_name, auto_delete=True)
+        await channel.declare_queue(queue_name, auto_delete=True)
+    finally:
+        await connection.close()
 
+
+async def notify_rabbitmq(evt, _):
+    connection = await aio_pika.connect_robust('amqp://guest:guest@127.0.0.1:55672/')
+    try:
+        queue_name = 'orthanc-events'
+        channel = await connection.channel()
         await channel.default_exchange.publish(
             aio_pika.Message(body=f'Hello {queue_name}'.encode()), routing_key=queue_name,
         )
+    finally:
+        connection.close()
 
+
+async def get_first_message(*_):
+    connection = await aio_pika.connect_robust('amqp://guest:guest@127.0.0.1:55672/')
+    try:
+        queue_name = 'orthanc-events'
+        channel = await connection.channel()
+        queue = await channel.get_queue(queue_name, ensure=False)
         messages = []
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    messages.append(message.body.decode())
+                    messages.append(message.body)
                     break
 
-        assert 'Hello orthanc-events' == messages[0]
+        return messages[0]
     finally:
         await connection.close()
+
+
+def test_registered_callback_should_be_notify_change_event(docker_rabbitmq, orthanc):
+    event_dispatcher.register_event_handlers({
+        orthanc.ChangeType.ORTHANC_STARTED: [create_queue],
+        orthanc.ChangeType.STABLE_STUDY: [notify_rabbitmq, get_first_message],
+    }, orthanc, httpx)
+
+    orthanc.on_change(
+        orthanc.ChangeType.ORTHANC_STARTED, orthanc.ResourceType.NONE, 'resource-uuid')
+
+    return_values = orthanc.on_change(
+        orthanc.ChangeType.STABLE_STUDY, orthanc.ResourceType.STUDY, 'resource-uuid')
+
+    assert return_values == [None, b'Hello orthanc-events']
