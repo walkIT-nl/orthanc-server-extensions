c435f17064fadecdb28f5b4ab5158fd5ce67299f
walcovanloon <walco@walkit.nl>
2023-01-30 11:09:21 +0100

    #131 Fix noisy auto-retries test by running resubmit job in same thread (for testing)

--- orthanc_ext/scripts/auto_retries.py
+++ orthanc_ext/scripts/auto_retries.py
@@ -18,13 +18,19 @@ def calculate_delay(job, first_retry=ONE_MINUTE):
     return min(max(first_retry, elapsed.seconds * 2), ONE_DAY)
 
 
-def resubmit_job(client, job_id, delay):
+def resubmit_job(client, job_id):
     resp = client.post(f'/jobs/{job_id}/resubmit')
     resp.raise_for_status()
     logging.info(f'resubmitted job "{job_id}"')
 
 
-def handle_failed_forwarding_job(first_retry=ONE_MINUTE, job_types=RETRYABLE_JOBTYPES):
+def python_timer_runner(job_id, delay, client):
+    timer = threading.Timer(interval=delay, function=resubmit_job, args=[client, job_id])
+    timer.start()
+
+
+def handle_failed_forwarding_job(
+        first_retry=ONE_MINUTE, job_types=RETRYABLE_JOBTYPES, job_runner=python_timer_runner):
 
     def handle_failed_forwarding_job(event, client):
         job_id = event.resource_id
@@ -37,7 +43,6 @@ def handle_failed_forwarding_job(first_retry=ONE_MINUTE, job_types=RETRYABLE_JOB
             return
         delay = calculate_delay(job, first_retry)
         logging.debug(f'resubmitting job "{job_id}" after {delay} seconds')
-        timer = threading.Timer(interval=delay, function=resubmit_job, args=[client, job_id, delay])
-        timer.start()
+        return job_runner(job_id, delay, client)
 
     return handle_failed_forwarding_job
