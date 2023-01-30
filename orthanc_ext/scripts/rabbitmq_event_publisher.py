e014fbeb733ac6153e1ab60ae5a3dccc7f8cb342
walcovanloon <walco@walkit.nl>
2023-01-30 11:17:11 +0100

    #131 Use partial application to configure RabbitMQ functions

--- orthanc_ext/scripts/rabbitmq_event_publisher.py
+++ orthanc_ext/scripts/rabbitmq_event_publisher.py
@@ -1,28 +1,30 @@
-import os
+from dataclasses import dataclass
 
 import aio_pika
 
 from orthanc_ext.scripts.event_publisher import convert_change_event_to_message
 
 
-async def create_queue(*_):
-    connection = await aio_pika.connect_robust(get_rabbitmq_url())
+@dataclass
+class RabbitmqConfig:
+    url: str
+    queue_name: str = 'orthanc-events'
+
+
+async def create_queue(rabbitmq_config, *_):
+    connection = await aio_pika.connect_robust(rabbitmq_config.url)
     try:
-        queue_name = 'orthanc-events'
+        queue_name = rabbitmq_config.queue_name
         channel = await connection.channel()
         await channel.declare_queue(queue_name, auto_delete=True)
     finally:
         await connection.close()
 
 
-def get_rabbitmq_url():
-    return os.getenv('RABBITMQ_URL', 'amqp://guest:guest@127.0.0.1:55672/')
-
-
-async def publish_to_rabbitmq(evt, *_):
-    connection = await aio_pika.connect_robust(get_rabbitmq_url())
+async def publish_to_rabbitmq(rabbitmq_config, evt, *_):
+    connection = await aio_pika.connect_robust(rabbitmq_config.url)
     try:
-        queue_name = 'orthanc-events'
+        queue_name = rabbitmq_config.queue_name
         channel = await connection.channel()
         _, message = convert_change_event_to_message(evt)
         await channel.default_exchange.publish(
--- tests/test_notifications_rabbitmq.py
+++ tests/test_notifications_rabbitmq.py
@@ -1,4 +1,5 @@
 import time
+from functools import partial
 
 import aio_pika
 import httpx
@@ -9,7 +10,13 @@ from orthanc_ext import event_dispatcher
 from orthanc_ext.http_utilities import create_internal_client, ClientType
 from orthanc_ext.orthanc import OrthancApiHandler
 from orthanc_ext.scripts.event_publisher import convert_message_to_change_event
-from orthanc_ext.scripts.rabbitmq_event_publisher import create_queue, publish_to_rabbitmq
+from orthanc_ext.scripts.rabbitmq_event_publisher import create_queue, publish_to_rabbitmq, \
+    RabbitmqConfig
+
+
+@pytest.fixture()
+def rabbitmq_config():
+    return RabbitmqConfig('amqp://guest:guest@127.0.0.1:55672/')
 
 
 @pytest.fixture(scope='session')
@@ -30,8 +37,8 @@ def docker_rabbitmq():
         yield container
 
 
-async def get_first_message(*_):
-    connection = await aio_pika.connect_robust('amqp://guest:guest@127.0.0.1:55672/')
+async def get_first_message(rabbitmq_config, *_):
+    connection = await aio_pika.connect_robust(rabbitmq_config.url)
     try:
         queue_name = 'orthanc-events'
         channel = await connection.channel()
@@ -48,10 +55,14 @@ async def get_first_message(*_):
         await connection.close()
 
 
-def test_registered_callback_should_be_notify_change_event(docker_rabbitmq, orthanc, async_client):
+def test_registered_callback_should_be_notify_change_event(
+        docker_rabbitmq, orthanc, async_client, rabbitmq_config):
     event_dispatcher.register_event_handlers({
-        orthanc.ChangeType.ORTHANC_STARTED: [create_queue],
-        orthanc.ChangeType.STABLE_STUDY: [publish_to_rabbitmq, get_first_message],
+        orthanc.ChangeType.ORTHANC_STARTED: [partial(create_queue, rabbitmq_config)],
+        orthanc.ChangeType.STABLE_STUDY: [
+            partial(publish_to_rabbitmq, rabbitmq_config),
+            partial(get_first_message, rabbitmq_config)
+        ],
     }, orthanc, httpx, async_client)
 
     orthanc.on_change(
