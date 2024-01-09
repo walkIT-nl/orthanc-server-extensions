b39457af2af0bd830710ffe894fa5ff8cb0f9f20
walcovanloon <walco@walkit.nl>
2024-01-09 19:50:44 +0100

    Upgrade all dev dependencies to fix dependabot alerts  * fix implicit import of kafka namespace by using the async admin client

--- orthanc_ext/scripts/kafka_event_publisher.py
+++ orthanc_ext/scripts/kafka_event_publisher.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 from aiokafka import AIOKafkaProducer
-from kafka.admin import KafkaAdminClient, NewTopic
+from aiokafka.admin import AIOKafkaAdminClient, NewTopic
 
 from orthanc_ext.scripts.event_publisher import convert_change_event_to_message
 
@@ -24,8 +24,11 @@ async def publish_to_kafka(kafka_config: KafkaConfig, evt, _):
         await producer.stop()
 
 
-def create_stream(kafka_config: KafkaConfig, *_):
-    admin_client = KafkaAdminClient(bootstrap_servers=kafka_config.bootstrap_server)
-    admin_client.create_topics(
-        new_topics=[NewTopic(name=kafka_config.topic, num_partitions=1, replication_factor=1)],
-        validate_only=False)
+async def create_stream(kafka_config: KafkaConfig, *_):
+    async with AIOKafkaAdminClient(bootstrap_servers=kafka_config.bootstrap_server,
+                                   request_timeout_ms=10000) as admin_client:
+        await admin_client.start()
+
+        await admin_client.create_topics(
+            new_topics=[NewTopic(name=kafka_config.topic, num_partitions=1, replication_factor=1)],
+            validate_only=False)
